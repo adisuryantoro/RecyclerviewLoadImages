093cadd34f0ae0c47de37148ff2095ef41979e71
Adi Suryantoro <adisuryantoro17@gmail.com>
2021-06-11 16:50:04 +0700

    add permission internet, allowBackup false, implementation in MainActivity

--- app/src/main/java/id/adisuryantoro/loadimage/view/MainActivity.kt
+++ app/src/main/java/id/adisuryantoro/loadimage/view/MainActivity.kt
@@ -2,11 +2,46 @@ package id.adisuryantoro.loadimage.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import id.adisuryantoro.loadimage.R
+import id.adisuryantoro.loadimage.databinding.ActivityMainBinding
+import id.adisuryantoro.loadimage.remote.WebService
+import id.adisuryantoro.loadimage.repository.MainRepository
+import id.betn2020.accordinnovations.viewmodel.MainViewModel
+import id.betn2020.accordinnovations.viewmodel.MyViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
+    private lateinit var binding: ActivityMainBinding
+
+    lateinit var viewModel: MainViewModel
+
+    private val webService = WebService.getInstance()
+    val adapter = MainAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this, MyViewModelFactory(MainRepository(webService))).get(MainViewModel::class.java)
+
+        binding.rvPhotos.adapter = adapter
+        binding.rvPhotos.layoutManager = GridLayoutManager(this,2)
+
+        viewModel.photosList.observe(this, Observer {
+            Log.d(TAG, "onCreate: $it")
+            adapter.setMovieList(it)
+        })
+
+        viewModel.errorMessage.observe(this, Observer {
+
+        })
+        viewModel.getAllPhotos()
     }
 }
