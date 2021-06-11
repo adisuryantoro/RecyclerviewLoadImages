5aa2cbd21badc3c5b12033e8c295e458ef8f7284
Adi Suryantoro <adisuryantoro17@gmail.com>
2021-06-11 16:17:01 +0700

    Initial commit

---
+++ app/src/main/java/id/adisuryantoro/loadimage/MainActivity.kt
@@ -0,0 +1,11 @@
+package id.adisuryantoro.loadimage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
